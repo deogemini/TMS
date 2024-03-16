c0d95e403d790b7cf98c8576236736ff31cfc029
deogemini <geminideogratias8@gmail.com>
2024-03-16 21:13:08 +0300

    🔨registration of vehicleForm

---
+++ database/migrations/2024_03_16_170722_create_vehicles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('type_id');
+            $table->string('make_model');
+            $table->string('vehicle_name');
+            $table->string('registration_number');
+            $table->string('engine_number');
+            $table->string('chasis_number');
+            $table->integer('year_manufacture');
+            $table->string('fuel_type');
+            $table->string('vehicle_image');
+            $table->integer('kilometres');
+            $table->string('status');
+            $table->timestamps();
+
+            $table->foreign('type_id')->references('id')->on('vehicle_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vehicles');
+    }
+};
--- resources/views/pages/truckspage.blade.php
+++ resources/views/pages/truckspage.blade.php
@@ -14,7 +14,7 @@
             <div class="col"></div>
             <div class="col-auto">
                             <button type="button" class="btn btn-success add-btn" data-bs-toggle="modal" data-bs-target="#showModal" >
-                                <i class="ri-add-line align-bottom me-1"></i> Add Vehicle Type
+                                <i class="ri-add-line align-bottom me-1"></i> Add New Vehicle
                             </button>
             </div>
         </div>
@@ -43,6 +43,14 @@
                 <td>{{$car->name}}</td>
                 <td></td>
                 <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
             </tr>
 
             @endforeach
@@ -62,17 +70,88 @@
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header bg-light p-3">
-                    <h5 class="modal-title" id="exampleModalLabel">Add Vehicle Type </h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Register Vehicle </h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
                         id="close-modal"></button>
                 </div>
                 <div class="card-body form-steps">
                     <form id="registration_form" action="{{ route('trucks.create') }}" method="post">
                         @csrf
-                        <div>
+                        <div class="row">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label class="form-label" for="description">Type Name</label>
+                                        <select name="type_id"  id="type_id" class="form-select">
+                                            <option value="" selected>Please Type Name</option>
+                                            @foreach ($cars as $item)
+                                                <option value="{{ $item->id }}">{{ $item->name }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label class="form-label" for="description">Vehicle Name</label>
+                                        <input type="text" class="form-control" name="name" placeholder="Enter Type name" required>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label class="form-label" for="description">Make/Model</label>
+                                        <input type="text" class="form-control" name="name" placeholder="Enter Type name" required>
+                                    </div>
+                                </div></div>
+                                <div class="col-md-6">
+                                    <div class="mb-3">
+                                        <label class="form-label" for="description">Registration Number</label>
+                                        <input type="text" class="form-control" name="name" placeholder="Enter Type name" required>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="mb-3">
+                                        <label class="form-label" for="description">Engine Number</label>
+                                        <input type="text" class="form-control" name="name" placeholder="Enter Type name" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="mb-3">
+                                        <label class="form-label" for="description">Chasis Number</label>
+                                        <input type="text" class="form-control" name="name" placeholder="Enter Type name" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="mb-3">
+                                        <label class="form-label" for="description">Year of Manufacture</label>
+                                        <select name="type_id"  id="type_id" class="form-select">
+                                            <option value="" selected>Please Type Name</option>
+                                            <option value="petrol">2000</option>
+                                            <option value="disel">2001</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                            </div>
+
                             <div class="mb-3">
-                                <label class="form-label" for="description">Type Name</label>
+                                <label class="form-label" for="description">Fuel Type</label>
+                                <select name="type_id"  id="type_id" class="form-select">
+                                    <option value="" selected>Please Type Name</option>
+                                    <option value="petrol">Petrol</option>
+                                    <option value="disel">Disel</option>
+                                </select>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label" for="description">Vehicle Images</label>
                                 <input type="text" class="form-control" name="name" placeholder="Enter Type name" required>
+                            </div>
+                            <div class="mb-3">
+                                <label class="form-label" for="description">Kilometers</label>
+                                <input type="number" class="form-control" name="name" placeholder="Enter Type name" required>
                             </div>
                             <p style="margin-top: 15px;"><b>NOTE: Fields marked with <span id="required-field">*</span> are mandatory</b></p>
                             <div class="d-flex align-items-start gap-3 mt-4">
