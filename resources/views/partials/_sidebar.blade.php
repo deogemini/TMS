7e8ba8525f86022e68d8cbc06ff8a94c6c86a260
mashely <ansherotimalnado10@gmail.com>
2024-03-30 01:43:11 +0300

    changes on sidebar

--- resources/views/partials/_sidebar.blade.php
+++ resources/views/partials/_sidebar.blade.php
@@ -8,7 +8,7 @@
           <!--change to offline or busy as needed-->
         </div>
         <div class="nav-profile-text d-flex flex-column">
-          <span class="font-weight-bold mb-2">Arnold Mashely</span>
+          <span class="font-weight-bold mb-2">Nordin Kamal</span>
           <span class="text-secondary text-small">Project Manager</span>
         </div>
         <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
